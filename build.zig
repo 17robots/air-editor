cee6c8a1161b99bc0c69aa2474ba71b88c68a81b
17robots <mdray@ameritech.net>
2025-01-06 13:08:19 -0500

    feat: initial commit

---
+++ build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const vaxis = b.dependency("vaxis", .{ .target = target, .optimize = optimize });
+
+    const exe = b.addExecutable(.{ .name = "vaxis-test", .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize });
+    exe.root_module.addImport("vaxis", vaxis.module("vaxis"));
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "run the app");
+    run_step.dependOn(&run_cmd.step);
+}
